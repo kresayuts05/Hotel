19383fc54b037195762f45d01f7905ceb6d60ab9
kresa tsvetkova <kresayuts@gmail.com>
2025-06-02 22:02:49 +0300

    made classes

---
+++ Room.h
@@ -0,0 +1,15 @@
+#pragma once
+class Room
+{
+public:
+	unsigned getNumber() const;
+	unsigned getCapacity() const;
+
+private:
+	unsigned number;
+	unsigned capacity;
+	bool has“errace = false;
+	bool hasBath = false;
+	bool isPremium = false;
+};
+
---
+++ SingleRoom.cpp
@@ -0,0 +1,1 @@
+#include "SingleRoom.h"
